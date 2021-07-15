46dd2dd2655ee968436f2f98d6efd066970e703b
Karl von Randow <karl@xk72.com>
2021-07-15 16:36:19 +1200

    Emit entrypoints.json in memory as well for webpack-dev-server compatibility

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const name = 'WebpackEntrypointsPlugin'
 const fs = require('fs');
-const { resolve } = require('path');
+const { dirname, relative, resolve } = require('path');
 const schema = require('./plugin.json');
 const { validate } = require('schema-utils');
+const webpack = require('webpack');
+const { RawSource } = webpack.sources || require('webpack-sources');
+
 class WebpackEntrypointsPlugin {
   constructor(options) {
     validate(schema, options, { name: 'Entrypoints Plugin' });
 
     this.path = options.path || 'entrypoints.json';
     this.change = options.change;
+    this.writeToFileEmit = options.writeToFileEmit || false;
   }
   apply(compiler) {
-    compiler.hooks.done.tap(name, (stats) => {
+    const emit = (compilation) => {
+      const stats = compilation.getStats();
       const chunkOnlyConfig = {
         assets: false,
         cached: false,
@@ -52,9 +57,26 @@ class WebpackEntrypointsPlugin {
           assets: entrypoints[en].assets,
         }
       }
-      fs.writeFileSync(path, JSON.stringify(data, null, '  '));
+      const output = JSON.stringify(data, null, '  ');
+      
+      const assetId = relative(compiler.options.output.path, path);
+      compilation.emitAsset(assetId, new RawSource(output));
+
+      if (this.writeToFileEmit) {
+        fs.mkdirSync(dirname(path), { recursive: true });
+        fs.writeFileSync(path, output);
+      }
+
       this.change && this.change(data);
-    })
+    };
+
+    if (webpack.version.startsWith('4')) {
+      compiler.hooks.emit.tap(name, emit);
+    } else {
+      compiler.hooks.thisCompilation.tap(name, (compilation) => {
+        compilation.hooks.afterProcessAssets.tap(name, () => emit(compilation));
+      });
+    }
   }
 }
 
